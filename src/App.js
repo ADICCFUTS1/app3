53545f8eb9ad4dd61a69e8686bbaffa83eee3c4c
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-03-25 00:38:39 -0300

    0100101

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./ruta/Home";
-import Second from "./ruta/Dos";
+import Dos from "./ruta/Dos";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dos" element={<Second />} />
+        <Route path="/dos" element={<Dos />} />
       </Routes>
     </>
   );
