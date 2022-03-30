2980cf2691dcb5f69e86eaefeac480601de31fc7
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-03-30 20:06:47 -0300

    011010

--- src/ruta/cards/Card1.js
+++ src/ruta/cards/Card1.js
@@ -21,31 +21,37 @@ export default function Card1() {
   const classes = useStyles();
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/img/qatar.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Sorteo Mundial 2022
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Sorteo
-        </Button>
-        <Button
-          size="small"
-          color="primary"
-          href="https://redcard-estadisticas.netlify.app/mundial-grupos"
-        >
-          Grupos
-        </Button>
-      </CardActions>
-    </Card>
+    <div>
+      <Typography gutterBottom variant="h5" component="h2">
+        Viernes 01/04/2022 {dta[14]}
+      </Typography>
+
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="/img/qatar.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              Sorteo Mundial 2022
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Sorteo
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            href="https://redcard-estadisticas.netlify.app/mundial-grupos"
+          >
+            Grupos
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
