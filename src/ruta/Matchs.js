c1453192d11fb36cb41c5f5ab386103018931eab
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-04-02 00:59:48 -0300

    1010010110

--- src/ruta/Matchs.js
+++ src/ruta/Matchs.js
@@ -49,17 +49,17 @@ export default function Matchs() {
   var lis = [];
 
   let BB = [
-    bloque.AIng[0],
+    bloque.AIng[0], //"",
+    "", //bloque.AIng[0],
+    "", //bloque.AIng[1],
     "",
+
+    bloque.AEsp[0], //"",
+    "", //bloque.AEsp[0],
     "",
     "",
 
-    bloque.AEsp[0],
-    "",
-    "",
-    "",
-
-    bloque.AAle[0],
+    bloque.AAle[0], //"",
 
     "",
     "",
