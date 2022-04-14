148f8d4f70ded3a76c7bc4c32975911fb712daf6
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-04-14 18:58:24 -0300

    10010101

--- public/match.js
+++ public/match.js
@@ -86,13 +86,13 @@ for (var i = 0; i <= ustat.length - 23; i++) {
 }
 
 let Enl = [
-  AEsp[0],
-  AIng[0],
+  "",
+  "",
 
   "",
 
-  "",
-  "",
+  Extra[0],
+  Extra[1],
   "",
   "",
   "",
--- src/ruta/Matchs.js
+++ src/ruta/Matchs.js
@@ -59,13 +59,13 @@ export default function Matchs() {
   var lis = [];
 
   let BB = [
-    bloque.AEsp[0],
-    bloque.AIng[0],
+    "",
+    "",
 
     "",
 
-    "",
-    "",
+    bloque.Extra[0],
+    bloque.Extra[1],
     "",
     "",
     "",
