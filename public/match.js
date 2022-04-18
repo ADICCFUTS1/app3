27e6e1f05f20cd637c562c93b6999f340f9cb31a
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-04-18 02:20:35 -0300

    1001010110

--- public/match.js
+++ public/match.js
@@ -1,24 +1,6 @@
 let Title = [
   "LaLiga",
-  "LaLiga",
-  "LaLiga",
-  "LaLiga",
-
-  "Bundesliga",
-
-  "FA CUP",
-
-  "Ligue 1",
-
-  "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
-  "Liga Profesional Argentina",
-
-  "Liga Dimayor Colombia",
-  "Liga Dimayor Colombia",
-  "Liga Dimayor Colombia",
+  "Serie A",
   "Liga Dimayor Colombia",
 
   /**/
@@ -32,53 +14,21 @@ let Title = [
   "ERROR"
 ];
 let Match = [
-  "Granada - Levante",
-  "Atlético Madrid - Espanyol",
-  "Athletic Club - Celta de Vigo",
-  "Sevilla - Real Madrid",
+  "Barcelona - Cádiz",
 
-  "Arminia Bielefeld - Bayern München",
+  "Napoli - Roma",
 
-  "Chelsea - Crystal Palace",
-
-  "PSG - Olympique Marseille",
-
-  "Defensa y Justicia - Atlético Tucumán",
-  "Godoy Cruz - Vélez Sarsfield",
-  "Argentinos Juniors - Talleres Córdoba",
-  "Boca Juniors - Lanús",
-  "Banfield - River Plate",
-
-  "Rionegro Águilas - Atlético Nacional",
-  "Deportivo Pereira - Millonarios",
-  "América de Cali - Deportivo Cali",
-  "Medellín - Unión Magdalena"
+  "Patriotas Boyacá - Envigado"
 ];
 
 var Est = [];
 
 let ustat = [
-  "https://es.fcstats.com/partido,granada,levante,509938.php",
-  "https://es.fcstats.com/partido,atletico-madrid,espanyol-barcelona,509934.php",
-  "https://es.fcstats.com/partido,athletic-bilbao,celta-de-vigo,509933.php",
-  "https://es.fcstats.com/partido,sevilla,real-madrid,509940.php",
+  "https://es.fcstats.com/partido,fc-barcelona,cadiz,509935.php",
 
-  "https://es.fcstats.com/partido,arminia-bielefeld,bayern-munchen,508891.php",
+  "https://es.fcstats.com/partido,napoli,as-roma,523607.php",
 
-  "https://es.fcstats.com/partido,chelsea-london,crystal-palace,561044.php",
-
-  "https://es.fcstats.com/partido,paris-saint-germain,olympique-marseille,508186.php",
-
-  "https://es.fcstats.com/partido,defensa-y-justicia,atletico-tucuman,549287.php",
-  "https://es.fcstats.com/partido,argentinos-juniors,talleres-de-cordoba,549281.php",
-  "https://es.fcstats.com/partido,godoy-cruz,velez-sarsfield,549289.php",
-  "https://es.fcstats.com/partido,boca-juniors,lanus,549284.php",
-  "https://es.fcstats.com/partido,banfield,river-plate,549282.php",
-
-  "https://es.fcstats.com/partido,rionegro-aguilas,atletico-nacional,547661.php",
-  "https://es.fcstats.com/partido,deportivo-pereira,millonarios,547669.php",
-  "https://es.fcstats.com/partido,america-de-cali,deportivo-cali,547662.php",
-  "https://es.fcstats.com/partido,independiente-medellin,union-magdalena,547664.php",
+  "https://es.fcstats.com/partido,patriotas,envigado-fc,547668.php",
 
   /** ***TEST*** */
   "https://pirlotv-redirecciones.blogspot.com/2020/09/estadisticas-no-disp.html",
@@ -113,24 +63,6 @@ for (var i = 0; i <= ustat.length - 23; i++) {
 }
 
 let Enl = [
-  "",
-  "",
-  "",
-  "",
-
-  "",
-
-  "",
-
-  "",
-
-  "",
-  "",
-  "",
-  "",
-  "",
-
-  "",
   "",
   "",
   "",
