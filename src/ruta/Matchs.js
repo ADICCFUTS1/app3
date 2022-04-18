d314405f02499d794ed48139da9d18009b4f4e9c
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-04-18 00:48:54 -0300

    1101010111

--- src/ruta/Matchs.js
+++ src/ruta/Matchs.js
@@ -74,12 +74,12 @@ export default function Matchs() {
     "",
     "",
     "",
-    bloque.Extra[0],
+    "",
 
     "",
     "",
-    bloque.ACol[0],
-    bloque.ACol[0],
+    "",
+    "",
 
     /****/
     "error!!1",
